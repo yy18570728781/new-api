7c34b804c3c4d4e8de8b1d919900ae99cba27e22
向晴 余友 <yy18570728781@163.com>
2026-05-14 01:03:47 +0800

    Refactor code structure for improved readability and maintainability

--- web/classic/src/index.jsx
+++ web/classic/src/index.jsx
@@ -20,6 +20,7 @@ For commercial licensing, please contact support@quantumnous.com
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import './semi.css';
 import { UserProvider } from './context/User';
 import 'react-toastify/dist/ReactToastify.css';
 import { StatusProvider } from './context/Status';
